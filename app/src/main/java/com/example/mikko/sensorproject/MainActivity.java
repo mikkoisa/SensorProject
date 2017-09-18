331963d46730fa8d722b1fc25ec1c67bbf4aaae3
Mikko <mikkoisa@metropolia.fi>
2017-09-18 09:16:50 +0300

    Second push

--- app/src/main/java/com/example/mikko/sensorproject/MainActivity.java
+++ app/src/main/java/com/example/mikko/sensorproject/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.mikko.sensorproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button TestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TestButton = (Button)findViewById(R.id.btnTest);
     }
 }
